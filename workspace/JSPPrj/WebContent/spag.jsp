1982f681eb2e98a3d584f21e01e8fed2b1503a29
minchang0116 <new0822@naver.com>
2020-09-09 00:08:20 +0900

    50. 수강

--- workspace/JSPPrj/WebContent/spag.jsp
+++ workspace/JSPPrj/WebContent/spag.jsp
@@ -8,7 +8,18 @@
 <meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
+<%
+pageContext.setAttribute("result", "hello");
+%>
+
 <body>
-	<%=request.getAttribute("result") %>입니다.
+	<%=request.getAttribute("result")%>입니다.
+	${requestScope.result}<br >
+	${names[1]}<br >
+	${notice.title}<br >
+	${result}<br >
+	${empty param.n? '값이 비어 있습니다.' : param.n }<br >
+	${param.n/2 }<br >
+	${header.accept}
 </body>
 </html>
--- workspace/JSPPrj/src/com/newlecture/web/Spag.java
+++ workspace/JSPPrj/src/com/newlecture/web/Spag.java
@@ -1,15 +1,18 @@
 package com.newlecture.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
 @WebServlet("/spag")
 public class Spag extends HttpServlet {
-	protected void doGet(javax.servlet.http.HttpServletRequest req, javax.servlet.http.HttpServletResponse resp)
+	protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
 			throws javax.servlet.ServletException, java.io.IOException {
 		int num = 0;
-		String num_ = req.getParameter("n");
+		String num_ = request.getParameter("n");
 		if (num_ != null && !num_.equals(""))
 			num = Integer.parseInt(num_);
 
@@ -20,11 +23,19 @@ public class Spag extends HttpServlet {
 		else
 			result = "짝수";
 		
-		req.setAttribute("result", result);
+		request.setAttribute("result", result);
+		
+		String[] names = {"newlec", "dragon"};
+		request.setAttribute("names", names);
+		
+		Map<String, Object> notice = new HashMap<String, Object>();
+		notice.put("id", 1);
+		notice.put("title", "EL은 좋아요");
+		request.setAttribute("notice", notice);
 		
 		// redirect : 현재와 상관없이 새로운 요청
 		// forward : 현재 작업한 내용을 이어갈 수 있도록 공유
-		RequestDispatcher dispatcher = req.getRequestDispatcher("spag.jsp");
-		dispatcher.forward(req, resp);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("spag.jsp");
+		dispatcher.forward(request, response);
 	};
 }
